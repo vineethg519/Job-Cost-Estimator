0d4ed61ec1fa67ee88255bcb55e9394a66e9d79b
SunilKumar <S528333@mail.nwmissouri.edu>
2016-11-17 19:19:44 -0600

    committed on 17thNov

--- controllers/estimatePartRoofing.js
+++ controllers/estimatePartRoofing.js
@@ -1,11 +1,16 @@
+// This model is managed by Team 3-6
+// Sangaraju SunilKumar
+// kuchana Deepak
+//pothireddy subbareddy
 var express = require('express');
 var api = express.Router();
 var find = require('lodash.find');
 var remove = require('lodash.remove');
 var findIndex = require('lodash.findindex');
-//Add a comment to this line
 var Model = require('../models/estimatePartRoofing.js');
 const notfoundstring = 'No such estimatePartRoofing';
+var express = require('express');
+var api = express.Router();
 
 
 // see app.js for the root request this controller handles
@@ -14,15 +19,108 @@ const notfoundstring = 'No such estimatePartRoofing';
 api.get("/", function (request, response) {
   response.render("roofing_cost/index.ejs");
 });
-
-api.get('/findall', function(req, res){
-res.setHeader('Content-Type', 'application/json');
-//Add a comment to this line
-var data = req.app.locals.roofingBasecoats.query;
-res.send(JSON.stringify(data));
+// GET create
+api.get("/create", function(req, res) {
+    console.log('Handling GET /create' + req);
+    res.render("roofing_cost/create.ejs",
+        { title: "WP Primers", layout: "layout.ejs" });
 });
 
-module.exports = api;
+// GET /delete/:id
+api.get('/delete/:id', function(req, res) {
+    console.log("Handling GET /delete/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartRoofings.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('roofing_cost/delete.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            waterproofingPrimer: item
+        });
+});
 
-// This model is managed by by team 03-R06
-// S.SunilKumar,K.Deepak
+// GET /details/:id
+api.get('/details/:id', function(req, res) {
+    console.log("Handling GET /details/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartRoofings.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('roofing_cost/details.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            waterproofingPrimer: item
+        });
+});
+
+// GET one
+api.get('/edit/:id', function(req, res) {
+    console.log("Handling GET /edit/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartRoofings.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('roofing_cost/edit.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            waterproofingPrimer: item
+        });
+});
+
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
+
+// POST new
+api.post('/save', function(req, res) {
+    console.log("Handling POST " + req);
+    var data = req.app.locals.estimatePartRoofings.query;
+    var item = new Model;
+    console.log("NEW ID " + req.body._id);
+    item._id = parseInt(req.body._id);
+    item.name = req.body.name;
+    item.unit = req.body.unit;
+    item.price = req.body.price;
+    item.displayorder = parseInt(req.body.displayorder);
+    data.push(item);
+    console.log("SAVING NEW ITEM " + JSON.stringify(item));
+    return res.redirect('/waterproofingPrimer');
+});
+
+// POST update
+api.post('/save/:id', function(req, res) {
+    console.log("Handling SAVE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling SAVING ID=" + id);
+    var data = req.app.locals.estimatePartRoofings.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("ORIGINAL VALUES " + JSON.stringify(item));
+    console.log("UPDATED VALUES: " + JSON.stringify(req.body));
+    item.name = req.body.name;
+    item.unit = req.body.unit;
+    item.price = req.body.price;
+    item.displayorder = req.body.displayorder;
+    console.log("SAVING UPDATED ITEM " + JSON.stringify(item));
+    return res.redirect('/waterproofingPrimer');
+});
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', function(req, res, next) {
+    console.log("Handling DELETE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling REMOVING ID=" + id);
+    var data = req.app.locals.estimatePartRoofings.query;
+    var item = remove(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("Deleted item " + JSON.stringify(item));
+    return res.redirect('/waterproofingPrimer');
+});
+
+
+module.exports = api;
