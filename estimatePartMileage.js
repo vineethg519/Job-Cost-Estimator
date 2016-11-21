69c194631461adf6491ab859f518ce761e7c1275
NWMSU\S528178 <S528178@mail.nwmissouri.edu>
2016-11-21 13:41:12 -0600

    final

---
+++ estimatePartMileage.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+    , EntryMileage = require('./entryMileage.js')
+
+var EstimatePartMileageSchema = new Schema({
+    dollarsPerMile: { type: Number, required: true, default: 0.54 },
+    entries: [{
+        entry: { type: Schema.Types.ObjectId, ref: EntryMileage },
+    }],
+    subtotal:  { type: Number, required: true, default: 0 }
+})
+
+var estimatePartMileage = mongoose.model('EstimatePartMileage', EstimatePartMileageSchema)
+module.exports = estimatePartMileage
+
+// This model is managed by Team 5-11
+// Monish Verma
+// Abhilash Pochampally
