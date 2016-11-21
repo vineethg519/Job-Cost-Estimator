4f37e8d5368207ef4d66683d1bb4f4b4351d9f3e
S528110 <S528110@mail.nwmissouri.edu>
2016-11-20 19:52:24 -0600

    in

--- controllers/estimatePartLabor.js
+++ controllers/estimatePartLabor.js
@@ -4,43 +4,142 @@ var find = require('lodash.find');
 var remove = require('lodash.remove');
 var findIndex = require('lodash.findindex');
 var Model = require('../models/estimatePartLabor.js');
-const notfoundstring = 'No such estimatePartLabor';
+const notfoundstring = 'No such estimate PartLabor';
+
 
 // See app.js to find default view folder (e.g.,"views")
-// see app.js to find  default URI for this controller (e.g., "estimatePartLabor")
+// see app.js to find  default URI for this controller (e.g., "waterproofingPrimer")
 // Specify the handler for each required combination of URI and HTTP verb 
 // HTML5 forms can only have GET and POST methods (use POST for DELETE)
 
+
 // HANDLE JSON REQUESTS --------------------------------------------
+
 api.get('/findall', function(req, res){
     res.setHeader('Content-Type', 'application/json');
     var data = req.app.locals.estimatePartLabors.query;
     res.send(JSON.stringify(data));
 });
 
+api.get('/findone/:id', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = data.find(function(dt){ { return dt._id==req.params.id; }});
+    res.send(JSON.stringify(item));
+});
+
+
+// see app.js for the root request this controller handles
+
 // GET to this controller root URI
 api.get("/", function (request, response) {
   response.render("labor_cost/index.ejs");
 });
 
-//GET create 
-api.get('/create', function (request, response) {
-   response.render("labor_cost/create.ejs");
+
+// GET create
+api.get("/create", function(req, res) {
+    console.log('Handling GET /create' + req);
+    res.render("labor_cost/create",
+        { title: "WP Primers", layout: "layout.ejs" });
 });
 
-//GET delete 
-api.get('/delete', function (request, response) {
-   response.render("labor_cost/delete.ejs");
+// GET /delete/:id
+api.get('/delete/:id', function(req, res) {
+    console.log("Handling GET /delete/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('labor_cost/delete.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            estimatePartLabor: item
+        });
 });
 
-//GET details 
-api.get('/details', function (request, response) {
-   response.render("labor_cost/details.ejs");
+// GET /details/:id
+api.get('/details/:id', function(req, res) {
+    console.log("Handling GET /details/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('labor_cost/details.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            estimatePartLabor: item
+        });
 });
 
-//GET edit 
-api.get('/edit', function (request, response) {
-   response.render("labor_cost/edit.ejs");
+// GET one
+api.get('/edit/:id', function(req, res) {
+    console.log("Handling GET /edit/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('labor_cost/edit.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            estimatePartLabor: item
+        });
+});
+
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
+
+// POST new
+api.post('/save', function(req, res) {
+    console.log("Handling POST " + req);
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = new Model;
+    console.log("NEW ID " + req.body._id);
+    item._id = parseInt(req.body._id);
+    item.name = req.body.name;
+    item.unit = req.body.unit;
+    item.price = req.body.price;
+    item.displayorder = parseInt(req.body.displayorder);
+    data.push(item);
+    console.log("SAVING NEW ITEM " + JSON.stringify(item));
+    return res.redirect('/estimatePartLabor');
+});
+
+// POST update
+api.post('/save/:id', function(req, res) {
+    console.log("Handling SAVE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling SAVING ID=" + id);
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("ORIGINAL VALUES " + JSON.stringify(item));
+    console.log("UPDATED VALUES: " + JSON.stringify(req.body));
+    item.type = req.body.type;
+    item.count = req.body.count;
+    item.hoursPerPerson = req.body.hoursPerPerson;
+    item.dollarsPerHour = req.body.dollarsPerHour;
+    item.nightsPerPerson = req.body.nightsPerPerson;
+    item.costPerNight	 = req.body.costPerNight;
+    console.log("SAVING UPDATED ITEM " + JSON.stringify(item));
+    return res.redirect('/estimatePartLabor');
+});
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', function(req, res, next) {
+    console.log("Handling DELETE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling REMOVING ID=" + id);
+    var data = req.app.locals.estimatePartLabors.query;
+    var item = remove(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("Deleted item " + JSON.stringify(item));
+    return res.redirect('/estimatePartLabor');
 });
 
 
